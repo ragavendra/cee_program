4d5c0600814fa3287c1f9d43c1bad7c45067e035
ragavendra <ragavendra.bn@gmail.com>
2017-12-20 02:51:39 -0800

    playing wwith pointers

--- linked_list.c
+++ linked_list.c
@@ -34,38 +34,78 @@ void push_head (node_t **head ,int value) {
 	*head = new_node;
 }
 
+//reverse the list
+void reverse (node_t **head) {
+
+	node_t *current = *head;
+	node_t *previous = NULL;
+	node_t *next = NULL;
+	
+	while (current != NULL) {
+
+		next = current->next;
+		current->next = previous;
+
+		previous = current;
+		current = next;
+	}
+
+	//new_list->next = NULL;
+
+	*head = previous;
+
+}
+
+//display list
+void display_list (node_t *head) {
+
+	int index = 0;
+
+	//prints the linked list
+	while (head != NULL) {
+		printf("%d\n", head->value);
+		head = head->next;
+		//printf("Hello");
+		index++;
+		if (index > 20)
+			break;
+	}
+
+}
+
 int main() {
 	
 	//allocate memory for the structure
-	node_t *linked_list = malloc(sizeof(node_t));
+	node_t *linked_list = NULL;
 
 	//copy variableas we will traverse linked_list
 	//and its address will change obviously
-	node_t *current = linked_list ;
+	node_t *current = NULL;
 
-	for (int index = 0; index < 10; index++) {
-		//current
-		linked_list->value = index;
+	linked_list = malloc(sizeof(node_t));
+	linked_list->value = 0;
+	current = linked_list;
+	
+	for (int index = 1; index < 10; ++index) {
 		linked_list->next = malloc(sizeof(node_t));
+		linked_list->next->value = index;
 		//printf("Hi %d", linked_list->value);
 		linked_list = linked_list->next;
 	}
-
-	//now points to the last structure
-	//linked_list = NULL;
-
+		
+	//display_list( current );
+	
 	//pass the whole structure head
 	push_last ( current, 36); 
 
 	//pass the address of the first structure
 	push_head ( &current, 48);
-	
-	//prints the linked list
-	while (current != NULL) {
-		printf("%d\n", current->value);
-		current = current->next;
-		//printf("Hello");
-	}
+
+	//lets reverse the list
+	reverse ( &current );
+
+	//display
+	display_list( current );
 
 	return 0;
 }
